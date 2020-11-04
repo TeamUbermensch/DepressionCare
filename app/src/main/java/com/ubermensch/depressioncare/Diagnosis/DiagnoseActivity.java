16a86c8794e72e8178315e7fd562b63207ffcf51
GiDiK <ubmsch28@naver.com>
2020-11-05 08:29:12 +0900

    test commit

--- app/src/main/java/com/ubermensch/depressioncare/Diagnosis/DiagnoseActivity.java
+++ app/src/main/java/com/ubermensch/depressioncare/Diagnosis/DiagnoseActivity.java
@@ -12,5 +12,7 @@ public class DiagnoseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diagnose);
+
+        // commit test
     }
 }
