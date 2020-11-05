3b39d28de7b48e5b93b808571408d3c11919c586
GiDiK <ubmsch28@naver.com>
2020-11-06 05:01:17 +0900

    Add Diagnose Layout(50%)

--- app/src/main/java/com/ubermensch/depressioncare/Diagnosis/DiagnoseActivity2.java
+++ app/src/main/java/com/ubermensch/depressioncare/Diagnosis/DiagnoseActivity2.java
@@ -1,22 +1,18 @@
 package com.ubermensch.depressioncare.Diagnosis;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.content.Intent;
 import android.os.Bundle;
-
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
 import com.ubermensch.depressioncare.R;
 
-public class DiagnoseActivity extends AppCompatActivity {
-
+public class DiagnoseActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_diagnose);
+        setContentView(R.layout.activity_diagnose2);
 
         Button button = (Button) findViewById(R.id.next_button);
 
@@ -24,10 +20,9 @@ public class DiagnoseActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), DiagnoseActivity2.class);
+                Intent intent = new Intent(getApplicationContext(), DiagnoseActivity3.class);
                 startActivity(intent);
             }
         });
     }
-
 }
---
+++ app/src/main/java/com/ubermensch/depressioncare/Diagnosis/DiagnoseActivity3.java
@@ -0,0 +1,13 @@
+package com.ubermensch.depressioncare.Diagnosis;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.ubermensch.depressioncare.R;
+
+public class DiagnoseActivity3 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diagnose3);
+    }
+}
